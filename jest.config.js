3e37788cdcb56d227571986034280fc7c8fd5dc1
Noah <noahnu@gmail.com>
2024-03-24 12:40:26 -0400

    chore: switch to jest multi-project setup (#63)

--- jest.config.js
+++ jest.config.js
@@ -2,30 +2,11 @@
 
 const CI = Boolean(process.env.CI)
 const ARTIFACT_DIR = process.env.ARTIFACT_DIR || 'artifacts'
-const IS_E2E = process.env.E2E === '1'
 
 /** @type {import('@jest/types').Config.InitialOptions} */
 const config = {
-    ...(CI && {
-        reporters: [
-            'default',
-            [
-                'jest-junit',
-                {
-                    suiteName: 'Jest Tests',
-                    outputDirectory: `${ARTIFACT_DIR}/test_results/jest/`,
-                    outputName: 'jest.junit.xml',
-                },
-            ],
-        ],
-        collectCoverage: true,
-    }),
-    transform: {
-        '^.+\\.tsx?$': require.resolve('ts-jest'),
-    },
     setupFiles: ['<rootDir>/testUtils/setup.ts'],
-    coverageReporters: CI ? ['json'] : ['text', 'json'],
-    coverageDirectory: 'raw-coverage/jest/',
+
     collectCoverageFrom: ['packages/**/src/**/*.ts', '.yarn/__virtual__/**/packages/**/*.ts'],
     coveragePathIgnorePatterns: ['/node_modules/', '/__mocks__/', '\\.test.ts$', '\\.mock.ts$'],
     watchPathIgnorePatterns: [
@@ -34,25 +15,50 @@ const config = {
         '<rootDir>/packages/.*/lib',
         '<rootDir>/packages/.*/.*\\.js',
     ],
-    testPathIgnorePatterns: [
-        '/node_modules/',
-        '/.yarn/',
-        '<rootDir>/.*\\.js',
-        '<rootDir>/.*/lib/',
-        ...(IS_E2E ? ['<rootDir>/packages'] : ['<rootDir>/e2e-tests']),
-    ],
+    transform: {
+        '^.+\\.tsx?$': [require.resolve('@swc/jest'), {}],
+    },
+    testPathIgnorePatterns: ['/node_modules/', '/.yarn/', '<rootDir>/.*\\.js', '<rootDir>/.*/lib/'],
     haste: {
         throwOnModuleCollision: true,
     },
     modulePathIgnorePatterns: ['<rootDir>/.*/lib'],
-    testTimeout: 30000,
     resolver: require.resolve('@tophat/jest-resolver'),
-    ...(IS_E2E && {
-        maxConcurrency: 1,
-        maxWorkers: 1,
-        testTimeout: 300000,
-        verbose: true,
-    }),
 }
 
-module.exports = config
+/** @type {import('@jest/types').Config.InitialOptions} */
+const multiProject = {
+    projects: [
+        {
+            ...config,
+            displayName: 'E2E',
+            testMatch: ['<rootDir>/e2e-tests/**/*.test.ts'],
+            globalSetup: '<rootDir>/e2e-tests/globalSetup.ts',
+        },
+        {
+            ...config,
+            displayName: 'Unit/Integration',
+            testMatch: ['<rootDir>/packages/**/*.test.ts'],
+        },
+    ],
+    testTimeout: 30000,
+    reporters: CI
+        ? [
+              'default',
+              [
+                  'jest-junit',
+                  {
+                      suiteName: 'Jest Tests',
+                      outputDirectory: `${ARTIFACT_DIR}/test_results/jest/`,
+                      outputName: 'jest.junit.xml',
+                  },
+              ],
+          ]
+        : ['default'],
+    watchPlugins: [require.resolve('jest-watch-select-projects')],
+    coverageReporters: CI ? ['json'] : ['text', 'json'],
+    coverageDirectory: 'raw-coverage/jest/',
+    collectCoverage: CI,
+}
+
+module.exports = multiProject
