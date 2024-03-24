d1e582d8b71ce2b505eed12144398fe7a79a016f
noahnu <noahnu@gmail.com>
2024-03-24 14:15:12 -0400

    fix: do not error on missing github token
    feat: support reading GITHUB_TOKEN env variable in @monoweave/plugin-github.

--- packages/plugin-github/src/plugin.test.ts
+++ packages/plugin-github/src/plugin.test.ts
@@ -1,4 +1,4 @@
-import { createMonorepoContext, getMonoweaveConfig } from '@monoweave/test-utils'
+import { createMonorepoContext, describeIf, getMonoweaveConfig } from '@monoweave/test-utils'
 import { type PluginHooks } from '@monoweave/types'
 import { AsyncSeriesHook } from 'tapable'
 
@@ -11,8 +11,15 @@ jest.mock('./request', () => ({
     request: jest.fn(),
 }))
 
-describe('GitHub Plugin', () => {
+describeIf(
+    /**
+     * Node lts/hydrogen has a bug (?) which causes jest to crash when using dynamic imports.
+     * https://stackoverflow.com/questions/77962982/testing-commonjs-with-dynamic-imports-of-esm-with-ts-jest#comment137891280_77962982
+     */
+    () => !process.env.NODE_VERSION?.includes('lts/hydrogen'),
+)('GitHub Plugin', () => {
     afterEach(() => {
+        delete process.env.GITHUB_TOKEN
         delete process.env.GH_TOKEN
         jest.resetAllMocks()
     })
@@ -37,10 +44,13 @@ describe('GitHub Plugin', () => {
         expect(info.name).toEqual(PluginName)
     })
 
-    it('throws an error if missing auth token', async () => {
+    it('logs an error if missing auth token and skips release', async () => {
         await using context = await createMonorepoContext({
             'pkg-1': {},
         })
+
+        const spyWarning = jest.spyOn(context.report, 'reportWarning')
+
         const config = {
             ...(await getMonoweaveConfig({
                 cwd: context.project.cwd,
@@ -49,16 +59,21 @@ describe('GitHub Plugin', () => {
             })),
         }
 
-        await expect(async () => {
-            await createPluginInternals({})(context, config, {
-                'pkg-1': {
-                    version: '1.0.0',
-                    tag: 'pkg-1@1.0.0',
-                    changelog: 'a new feature',
-                    group: 'pkg-1',
-                },
-            })
-        }).rejects.toThrow(/Missing GitHub Personal Access Token/)
+        const spyRequest = jest.spyOn(requestModule, 'request')
+        await createPluginInternals({})(context, config, {
+            'pkg-1': {
+                version: '1.0.0',
+                tag: 'pkg-1@1.0.0',
+                changelog: 'a new feature',
+                group: 'pkg-1',
+            },
+        })
+
+        await expect(spyWarning).toHaveBeenCalledWith(
+            expect.anything(),
+            expect.stringMatching(/Missing GitHub Personal Access Token/),
+        )
+        expect(spyRequest).not.toHaveBeenCalled()
     })
 
     it('throws an error if cannot determine github owner or repository', async () => {
@@ -68,6 +83,9 @@ describe('GitHub Plugin', () => {
             },
             { root: { repository: 'corrupted repository' } },
         )
+
+        const spyError = jest.spyOn(context.report, 'reportError')
+
         const config = {
             ...(await getMonoweaveConfig({
                 cwd: context.project.cwd,
@@ -76,18 +94,66 @@ describe('GitHub Plugin', () => {
             })),
         }
 
-        process.env.GH_TOKEN = 'abc'
+        process.env.GITHUB_TOKEN = 'abc'
 
-        await expect(async () => {
-            await createPluginInternals({})(context, config, {
+        await createPluginInternals({})(context, config, {
+            'pkg-1': {
+                version: '1.0.0',
+                tag: 'pkg-1@1.0.0',
+                changelog: 'a new feature',
+                group: 'pkg-1',
+            },
+        })
+
+        expect(spyError).toHaveBeenCalledWith(
+            expect.anything(),
+            expect.stringMatching(/Cannot determine GitHub owner/),
+        )
+    })
+
+    it('uses repository info from workspace over root if available', async () => {
+        await using context = await createMonorepoContext(
+            {
                 'pkg-1': {
-                    version: '1.0.0',
-                    tag: 'pkg-1@1.0.0',
-                    changelog: 'a new feature',
-                    group: 'pkg-1',
+                    raw: {
+                        repository: 'https://github.com/example/repo.git',
+                    },
                 },
-            })
-        }).rejects.toThrow(/Cannot determine GitHub owner/)
+            },
+            { root: { repository: 'https://github.com/other/other.git' } },
+        )
+
+        const spyError = jest.spyOn(context.report, 'reportError')
+
+        const config = {
+            ...(await getMonoweaveConfig({
+                cwd: context.project.cwd,
+                baseBranch: 'main',
+                commitSha: 'shashasha',
+            })),
+        }
+
+        process.env.GITHUB_TOKEN = 'abc'
+
+        const spyRequest = jest.spyOn(requestModule, 'request')
+        await createPluginInternals({})(context, config, {
+            'pkg-1': {
+                version: '1.0.0',
+                tag: 'pkg-1@1.0.0',
+                changelog: 'a new feature',
+                group: 'pkg-1',
+            },
+        })
+
+        expect(spyError).not.toHaveBeenCalled()
+        expect(spyRequest).toHaveBeenCalledWith(
+            expect.anything(),
+            expect.any(String),
+            expect.objectContaining({
+                owner: 'example',
+                repo: 'repo',
+            }),
+        )
     })
 
     it('skips releases if no changelog available and includeImplicitUpdates is disabled', async () => {
@@ -105,7 +171,7 @@ describe('GitHub Plugin', () => {
             })),
         }
 
-        process.env.GH_TOKEN = 'abc'
+        process.env.GITHUB_TOKEN = 'abc'
 
         const spyRequest = jest.spyOn(requestModule, 'request')
         await createPluginInternals({ includeImplicitUpdates: false })(context, config, {
@@ -134,7 +200,7 @@ describe('GitHub Plugin', () => {
             })),
         }
 
-        process.env.GH_TOKEN = 'abc'
+        process.env.GITHUB_TOKEN = 'abc'
 
         const spyRequest = jest.spyOn(requestModule, 'request')
         await createPluginInternals({ includeImplicitUpdates: true })(context, config, {
@@ -173,7 +239,7 @@ describe('GitHub Plugin', () => {
                 dryRun: true,
             })),
         }
-        process.env.GH_TOKEN = 'abc'
+        process.env.GITHUB_TOKEN = 'abc'
 
         const spyRequest = jest.spyOn(requestModule, 'request')
         await createPluginInternals({})(context, config, {
@@ -202,9 +268,12 @@ describe('GitHub Plugin', () => {
             })),
         }
 
-        process.env.GH_TOKEN = 'abc'
+        // standard recommended env var
+        process.env.GITHUB_TOKEN = 'abc'
 
         const spyRequest = jest.spyOn(requestModule, 'request')
+        const spyWarning = jest.spyOn(context.report, 'reportWarning')
+        const spyError = jest.spyOn(context.report, 'reportError')
         await createPluginInternals({})(context, config, {
             'pkg-1': {
                 version: '1.0.0',
@@ -224,5 +293,44 @@ describe('GitHub Plugin', () => {
                 body: 'a new feature',
             }),
         )
+
+        // No warnings or errors
+        expect(spyWarning).not.toHaveBeenCalled()
+        expect(spyError).not.toHaveBeenCalled()
+    })
+
+    it('defaults to GH_TOKEN if GITHUB_TOKEN not specified', async () => {
+        await using context = await createMonorepoContext(
+            {
+                'pkg-1': {},
+            },
+            { root: { repository: 'https://github.com/example/repo.git' } },
+        )
+        const config = {
+            ...(await getMonoweaveConfig({
+                cwd: context.project.cwd,
+                baseBranch: 'main',
+                commitSha: 'shashasha',
+            })),
+        }
+
+        process.env.GH_TOKEN = 'abc'
+
+        const spyRequest = jest.spyOn(requestModule, 'request')
+        const spyWarning = jest.spyOn(context.report, 'reportWarning')
+        const spyError = jest.spyOn(context.report, 'reportError')
+        await createPluginInternals({})(context, config, {
+            'pkg-1': {
+                version: '1.0.0',
+                tag: 'pkg-1@1.0.0',
+                changelog: 'a new feature',
+                group: 'pkg-1',
+            },
+        })
+        expect(spyRequest).toHaveBeenCalled()
+
+        // No warnings or errors
+        expect(spyWarning).not.toHaveBeenCalled()
+        expect(spyError).not.toHaveBeenCalled()
     })
 })
