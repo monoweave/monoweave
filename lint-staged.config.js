5e6318eaaccc51cfd329addf18d6293fa541a5c3
noahnu <noahnu@gmail.com>
2024-04-20 13:21:34 -0400

    chore: remove vulnerable yaml-validator

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,9 +1,5 @@
 module.exports = {
     '{package.json,yarn.lock}': () => ['yarn dedupe', 'yarn constraints --fix'],
-    '*.{yml,yaml}': (filenames) => {
-        const files = filenames.join(' ')
-        return `yarn yaml-validator ${files}`
-    },
     '*.ts': (filenames) => {
         const files = filenames.join(' ')
         return [
